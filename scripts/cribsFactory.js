291953b7d3d23000d197f40affbb8fd9ad099e1b
zorro76 <sergey.martynuk@gmail.com>
2016-02-06 15:34:16 +0200

    create user interface

--- scripts/cribsFactory.js
+++ scripts/cribsFactory.js
@@ -5,31 +5,10 @@
 
 angular
 	.module('ngCribs')
-	.factory('cribsFactory', function(){
-
-		var cribsData = [
-			{
-				"type": "Condo",
-				"price": 220000,
-				"address": "213 Grove Street",
-				"description": "Excellent place, really nice view!"
-			},
-			{
-				"type": "House",
-				"price": 410500,
-				"address": "7823 Winding Way",
-				"description": "Beautiful home with lots of space for a large family"
-			},
-			{
-				"type": "Duplex",
-				"price": 395000,
-				"address": "834 River Lane",
-				"description": "Great neighbourhood and lot's of nice green space"
-			}
-		];
+	.factory('cribsFactory', function($http){
 
 		function getCribs() {
-			return cribsData;
+			return $http.get('data/data.json');
 		}
 
 		return {
---
+++ scripts/cribsFilter.js
@@ -0,0 +1,25 @@
+/**
+ * Created by martynuk on 06.02.16.
+ */
+angular
+	.module('ngCribs')
+	.filter('cribsFilter', function() {
+
+		return function(listings, priceInfo) {
+
+			var filtered = [];
+
+			var min = priceInfo.min;
+			var max = priceInfo.max;
+
+			angular.forEach(listings, function(listing) {
+
+				if(listing.price >= min && listing.price <= max) {
+
+					filtered.push(listing);
+				}
+			});
+
+			return filtered;
+		}
+	});
